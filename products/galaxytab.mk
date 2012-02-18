06abc466829e27b3563c069e5f6cff2ec7b615ae
spanner3003 <spanner3003@gmail.com>
2012-02-18 12:15:33 +0000

    add galaxytab "7 (GSM)

---
+++ products/galaxytab.mk
@@ -0,0 +1,20 @@
+$(call inherit-product, device/samsung/galaxytab/full_galaxytab.mk)
+
+$(call inherit-product, vendor/aokp/configs/common_tablet.mk)
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/galaxytab
+
+PRODUCT_NAME := aokp_galaxytab
+PRODUCT_DEVICE := galaxytab
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := GT-P1000
+PRODUCT_RELEASE_NAME := GalaxyTab
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-P1000 TARGET_DEVICE=GT-P1000 BUILD_ID=GINGERBREAD BUILD_FINGERPRINT=samsung/GT-P1000/GT-P1000:2.3.5/GINGERBREAD/XWJQ3:user/release-keys PRIVATE_BUILD_DESC="GT-P1000-user 2.3.5 GINGERBREAD XWJQ3 release-keys"
+
+# Inherit common build.prop overrides
+-include vendor/aokp/configs/common_versions.mk
+
+# Copy maguro specific prebuilt files
+PRODUCT_COPY_FILES += \
+	vendor/aokp/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip \
